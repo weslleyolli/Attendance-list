341c0c72b5d22f3f1ddc9746b51e1d3ae897b042
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-30 18:52:44 -0300

    fragments

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import "./style/global.css"
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-500">Attendance list</h1>
+      <h1>Attendance list</h1>
+      <input type="text" placeholder="Enter your name..." />
+      <button type="button">Add</button>
     </div>
   )
 }
