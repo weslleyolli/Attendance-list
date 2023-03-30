1d618e2b106b4bf98a42901d4e9429e540876766
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-30 18:44:04 -0300

    files and archives

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./style/global.css"
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-500">Hello world</h1>
+      <h1 className="text-red-500">Attendance list</h1>
     </div>
   )
 }
