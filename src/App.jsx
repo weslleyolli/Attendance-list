a0b65c2af89cf5a2d1909552f20bb02aeba31914
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-30 23:46:04 -0300

    stilized input

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import "./style/global.css"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Attendance list</h1>
-      <input type="text" placeholder="Enter your name..." />
+    <div className="flex items-center flex-col">
+      <h1 className="m-20 mb-6">Attendance list</h1>
+      <input className="w-1/2 p-6 bg-slate-300 rounded border-0" type="text" placeholder="Enter your name..." />
       <button type="button">Add</button>
     </div>
   )
