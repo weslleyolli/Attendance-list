62f1acf64a634d31bd9e643acec4595841622161
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-04-01 20:17:29 -0300

    useStates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import React, {useState} from "react"
+
 import { Card } from "./components/Card"
 import "./style/global.css"
 
 function App() {
+  const [studentName, setStudentName] = useState()
   return (
     <div className="flex items-center flex-col">
-      <h1 className="m-20 mb-6 font-bold text-2xl" >Attendance list</h1>
-      <input className="w-1/2 p-6 bg-slate-300 rounded border-0 text-base" type="text" placeholder="Enter your name..." />
+      <h1 className="m-20 mb-6 font-bold text-2xl" >Name: {studentName}</h1>
+      <input 
+        className="w-1/2 p-6 bg-slate-300 rounded border-0 text-base" 
+        type="text" 
+        placeholder="Enter your name..." 
+        onChange={e => setStudentName(e.target.value)}
+      />
       <button 
         className="w-1/2 p-6 font-bold bg-red-400 text-white rounded mt-3 mb-20 hover:bg-red-500 transition duration-200 ease-in-out text-base" 
         type="button"
