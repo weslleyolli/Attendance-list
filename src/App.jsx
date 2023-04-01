2b9baf92e979a03120a5e6949286e9446b8ab4a2
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-03-31 21:44:25 -0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import { Card } from "./components/Card"
 import "./style/global.css"
 
 function App() {
   return (
     <div className="flex items-center flex-col">
-      <h1 className="m-20 mb-6">Attendance list</h1>
+      <h1 className="m-20 mb-6 font-bold text-2xl" >Attendance list</h1>
       <input className="w-1/2 p-6 bg-slate-300 rounded border-0 text-base" type="text" placeholder="Enter your name..." />
       <button 
         className="w-1/2 p-6 font-bold bg-red-400 text-white rounded mt-3 mb-20 hover:bg-red-500 transition duration-200 ease-in-out text-base" 
         type="button"
       >Add</button>
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function Card() {
+    return(
+        <div className="h-24 w-1/2 bg-blue-600 text-white rounded-lg mb-5 flex items-center justify-between p-6">
+            <strong className="text-lg">Weslley Oliveira</strong>
+            <small>10:14:24</small>
+        </div>
+    )
+}
